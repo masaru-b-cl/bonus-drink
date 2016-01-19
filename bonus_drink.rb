01a814e4655770ef80e2b6a1be02f7c645915343
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2016-01-20 00:26:01 +0900

    implements answers and tests

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,16 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+#   bonus_1 = amount / 3
+#   total_count = amount + bonus_1
+#   total_count += total_count / 3 - bonus_1
+#   total_count
+    # this expression is transformed from avove statements
+    # total_count = amount + amount / 3 + (amount + (amount / 3)) / 3 - amount / 3
+    #             = amount + amount / 3 + (amount / 3) / 3
+    #             = ( 3 * amount + amount + amount / 3 ) / 3
+    #             = ( 4 * amount + amount / 3 ) / 3
+    #             = ( 3 * 4 * amount + 3 * amount / 3 ) / 3 * 3
+    #             = ( 12 * amount + amount ) / 9
+    13 * amount / 9
   end
 end
